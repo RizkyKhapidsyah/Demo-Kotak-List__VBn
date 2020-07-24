9d29ce2cb9351d32584d9f9d1d2c51e71c0d1cd0
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2020-07-24 22:07:37 +0700

    Add project files.

---
+++ Demo Kotak List/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listAnjing = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.listKucing = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'listAnjing
+        '
+        Me.listAnjing.FormattingEnabled = True
+        Me.listAnjing.Location = New System.Drawing.Point(76, 29)
+        Me.listAnjing.Name = "listAnjing"
+        Me.listAnjing.Size = New System.Drawing.Size(120, 95)
+        Me.listAnjing.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(73, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Anjing:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(73, 139)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Kucing:"
+        '
+        'listKucing
+        '
+        Me.listKucing.FormattingEnabled = True
+        Me.listKucing.Location = New System.Drawing.Point(76, 155)
+        Me.listKucing.Name = "listKucing"
+        Me.listKucing.Size = New System.Drawing.Size(120, 43)
+        Me.listKucing.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.listKucing)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.listAnjing)
+        Me.Name = "Form1"
+        Me.Text = "Demo Kotak List"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents listAnjing As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents listKucing As System.Windows.Forms.ListBox
+
+End Class
---
+++ Demo Kotak List/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        listAnjing.Items.Add("Poodle")
+        listAnjing.Items.Add("Great Dane")
+        listAnjing.Items.Add("German Shepherd")
+        listAnjing.Items.Add("Terrier")
+
+        listKucing.Items.Add("Siamese")
+        listKucing.Items.Add("Persian")
+        listKucing.Items.Add("Bobtail")
+        listKucing.Items.Add("Burmese")
+    End Sub
+End Class
